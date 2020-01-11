3b7d03a1a808257aa3f6af1a11c9aabd0515bdb6
Eric Wooley <ericwooley@gmail.com>
2020-01-11 14:27:40 -0800

    get options

--- src/typeschema.ts
+++ src/typeschema.ts
@@ -1,6 +1,31 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// #! /usr/bin/env node
 
+import find from 'find'
+
+const argv = process.argv.slice(1)
+const dirs = argv.filter(arg => arg.indexOf('-') !== 0)
+const flags = argv.filter(arg => arg.indexOf('-') === 0)
+function extractOption(option: string) {
+  const val = option.split('=')[1]
+  if (!val) throw new Error('malformed flag: ' + option)
+  return val
+}
+const defaultPattern = '--pattern=\\.schema\\.(json|yml)'
+const options = {
+  dirs,
+  flags,
+  watch: !!flags.find(arg => arg === '--watch' || arg === '-w'),
+  pattern: extractOption(
+    flags.find(arg => arg.indexOf('--pattern=') === 0 || arg.indexOf('-p=') === 0) || defaultPattern
+  )
+}
+console.log(options)
+async function main() {
+  find.file(/\.js$/, __dirname, function(files) {
+    console.log(files.length)
+  })
+}
+
+if (process.argv[0] === __filename) {
+  main().catch(e => console.error(e))
 }
